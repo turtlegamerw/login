4cf6b34a8394c2dc4dbaf74a167a026a1ad9b883
turtlegamerw <popperex9@gmail.com>
2023-04-22 15:23:16 +0200

    yessir baby
    made it work

--- LoginSystem/src/Encryptor.java
+++ LoginSystem/src/Encryptor.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+import java.util.Scanner;
 
 public class Encryptor {
     public String encryptString(String input) throws NoSuchAlgorithmException {
@@ -18,8 +18,21 @@ public class Encryptor {
     public static void main(String[] args) throws NoSuchAlgorithmException {
         Encryptor encryptor = new Encryptor();
         
-        String password = "mika likes skidding fr";
-        
-        System.out.println(encryptor.encryptString(password));
+        //String password = "turtle";
+        //System.out.println(encryptor.encryptString(password));
+        String hashedpass = "d4705b9f42c96eeb0b9fb53266013516";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("pls enter your password:");
+
+        String userinput = scanner.nextLine();
+
+        if (encryptor.encryptString(userinput).equals(hashedpass)) {
+            System.out.println("correct password your in!");
+        }
+        else {
+            System.out.println("L you got it wrong bozo fr kill yourself");
+        }
     }
+
 }
