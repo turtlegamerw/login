e6b6390ea331435896e929a7933bf46ec265dfe1
turtlegamerw <popperex9@gmail.com>
2023-04-22 14:47:50 +0200

    skidded this part fr
    sgdfgsdf

---
+++ LoginSystem/src/Encryptor.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class Encryptor {
+    public String encryptString(String input) throws NoSuchAlgorithmException {
+
+        //does some stupid math
+        MessageDigest md = MessageDigest.getInstance("md5");
+
+        byte[] messageDigest = md.digest(input.getBytes());
+
+        BigInteger bigInt = new BigInteger(1, messageDigest);
+        return bigInt.toString(16);
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Encryptor encryptor = new Encryptor();
+        
+        String password = "mika likes skidding fr";
+        
+        System.out.println(encryptor.encryptString(password));
+    }
+}
